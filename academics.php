77c02771a70f516e8d2f5b97f9206a209b97520a
NirzorSen <124883741+NirzorSen@users.noreply.github.com>
2023-02-09 18:03:39 +0600

    Add files via upload

---
+++ academics.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academics</title>
+</head>
+<body>
+	<?php include('common/header.php') ?>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Engineering and Technology </h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+						It gives me great pleasure, as Head of the Engineering & Technology Department, to welcome you to the official Department of Engineering,
+                        This website has been developed with view of providing for staff, parents, students, and visitors to the site, a valuable source of news and timely information. It is to enable everyone to become familiar with the Department, its aim and vision, as well as to provide you with updated news and events. 
+						</p>
+						<p>
+						Department of Engineering has over two decades maintained a reputation of quality training and has been the forefront of preparing and providing competent middle level manpower to support the technological and engineering advancement beyond. Our well groomed staff takes great pride in providing a competent base mode of training, in a secure learning environment to enable our students to grow their professional career intellectually, emotionally and socially, while at the same time offering more interesting, exciting and challenging academic programs. The surest way to appreciate what we have to offer is to apply for one of our interesting programmes.
+						</p>
+						<p>
+							The qualiﬁed, dedicated and competent faculty, well equipped laboratories, classrooms with teaching aids adds value to the courses being conducted by the Departments. Campus is centrally located on the highly green canal road and can easily be approached from any corner of Lahore. The infra-structure and the atmosphere combine to provide great working and studying environment. Welcome to the most fulﬁlling and learning years of your life.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Art, Social Sciences and Humanities</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+						The Division of Humanities and Social Sciences fosters critical thinking, creative expression, and intellectual curiosity. In classrooms, laboratories, studios, and on stage, students are prepared for a variety careers, and they are equipped to prosper in an unpredictable future. I am proud to lead this Division as Dean, and I am dedicated to the belief that the programs offered in the Humanities and Social Sciences are the best preparation for the fierce urgency of the contemporary world.
+						</p>
+						<p>
+						Our division includes nine departments: English, History, Media Culture, Performing and Creative Arts, Philosophy, Political Science and Global Affairs, Psychology, Sociology and Anthropology, and World Languages and Literatures. The division also includes the major in Science, Letters, and Society; Interdisciplinary majors and minors in African and African Diaspora Studies, American Studies, International Studies; and Women’s, Gender, and Sexuality Studies; as well as minors in Disability Studies, East Asian Studies, Latin American, Caribbean, and Latina/o Studies, and a Graduate Certificate in Autism Spectrum Disorders.   In total, the academic departments and ten interdisciplinary programs provide a rich curriculum to all 14,000 students enrolled at CSI through general education, baccalaureate majors, minors, associate, certificate and graduate programs.
+
+						</p>
+						<p>
+							The qualiﬁed, dedicated and competent faculty, well equipped laboratories, classrooms with teaching aids adds value to the courses being conducted by the Departments. Campus is centrally located on the highly green canal road and can easily be approached from any corner of Lahore. The infra-structure and the atmosphere combine to provide great working and studying environment. Welcome to the most fulﬁlling and learning years of your life.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Computer Sciences</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+						Welcome to the school of Computer Science and Information Technology.
+
+                        The institute has a sprawling area of 13.90 acres and lush green ambience all around. It is developing as the Centre of Excellence imparting quality technical education and generating competent professionals with a high degree of credibility, integrity, and ethical standards. We aim at recognizing, respecting, and nurturing the creative potential of each student.
+
+						</p>
+						<p>
+						We are committed to excellence through innovations in the teaching and learning process, and have been successful in maintaining good student enrolments while maintaining high academic standards. Our faculty are highly qualified, committed, and experienced, and we admit some of the best students in the country in Computer Science and Engineering, Information Technology and Master in Computer Applications (MCA). Our faculty and staff are committed to remaining technically current with a consistent aim of providing latest and relevant education to our students.
+						</p>
+						<p>
+							The qualiﬁed, dedicated and competent faculty, well equipped laboratories, classrooms with teaching aids adds value to the courses being conducted by the Departments. Campus is centrally located on the highly green canal road and can easily be approached from any corner of Lahore. The infra-structure and the atmosphere combine to provide great working and studying environment. Welcome to the most fulﬁlling and learning years of your life.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Business Administration</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+						Welcome to the Faculty of Business Administration. The Faculty of Business Administration constantly adapts to changes in the times and is fostering business leaders who will be engaged globally in the new era. Learning business administration leads to acquiring the most useful practical skills in the fields.
+						</p>
+						<p>
+						Business administration is a system of scientific knowledge of concerned planning and managing of organizational activities. The field extends to business management theory, accounting science, commerce, business engineering, management informatics, and analyzes management, taking a variety of approaches such as economics, sociology, psychology, and mathematics. It is a discipline that makes use of all academic disciplines and is useful for better organizational management. To that end, we have adopted a wide range of activities and learning methods that learn independently with small sized numbers in each lesson.
+						</p>
+						<p>
+							The qualiﬁed, dedicated and competent faculty, well equipped laboratories, classrooms with teaching aids adds value to the courses being conducted by the Departments. Campus is centrally located on the highly green canal road and can easily be approached from any corner of Lahore. The infra-structure and the atmosphere combine to provide great working and studying environment. Welcome to the most fulﬁlling and learning years of your life.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,20 @@
+
+	<?php include('common/header.php') ?>
+	<div class="home-div"></div>
+		<div class="w-1000 in-ad-ap">
+			<div class="row m-auto text-center">
+			
+				<div class="col-md-12"><h3>Brainware University</h3></div>
+				
+			</div>
+		</div>
+		<div class="paragraph">
+			<p>
+			Brainware University provides a harmonious environment and learning opportunities  to its students regardless of their gender, socioeconomic background, religious beliefs and regional differences.
+			</p>
+		</div>
+	<?php include('common/cards.php') ?>
+	<?php include('common/footer.php') ?>
+</body>
+</html>
+
